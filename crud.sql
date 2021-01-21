077437c06307ace9ac3c4821ae1bbc93ed735491
Jose Solis <jose_solis16@yahoo.com>
2021-01-21 14:52:18 -0600

    Version_de_Prueba

---
+++ crud.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-01-2021 a las 21:12:28
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `apellidos` text NOT NULL,
+  `telefono` text NOT NULL,
+  `ciudad` text NOT NULL,
+  `correo` text NOT NULL,
+  `contraseña` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `telefono`, `ciudad`, `correo`, `contraseña`) VALUES
+(0, 'Jose ', 'Solis', '888888', 'Jinotepe', 'jose_solis16@yahoo.com', ''),
+(0, 'Alejandra ', 'Guevara', '9999999', 'Jinotepe', 'jose_solis16@yahoo.com', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+	include_once 'conexion.php';
+
+	$sentencia_select=$con->prepare('SELECT *FROM clientes ORDER BY id DESC');
+	$sentencia_select->execute();
+	$resultado=$sentencia_select->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>EXAMEN_MULTIMEDIA_II</title>
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+	<div class="contenedor">
+		<h2>BIENVENIDO PROF TICAY!</h2>
+		<h2>*****Por Favor cree un nuevo usuario*****</h2>
+		<div class="barra__buscador">
+			<form action="" class="formulario" method="post">
+				<input type="text" name="buscar" placeholder="Buscar" 
+				value="<?php if(isset($buscar_text)) echo $buscar_text; ?>" class="input__text">
+				<input type="submit" class="btn" name="btn_buscar" value="Buscar">
+				<a href="insert.php" class="btn btn__nuevo">Nuevo Usuario</a>
+			</form>
+		</div>
+		<table>
+			<tr class="head">
+				<td>Id</td>
+				<td>Nombre</td>
+				<td>Apellidos</td>
+				<td>Teléfono</td>
+				<td>Ciudad</td>
+				<td>Correo</td>
+				<td>Contraseña</td>
+				<td colspan="2">Acción</td>
+			</tr>
+			<?php foreach($resultado as $fila):?>
+				<tr >
+					<td><?php echo $fila['id']; ?></td>
+					<td><?php echo $fila['nombre']; ?></td>
+					<td><?php echo $fila['apellidos']; ?></td>
+					<td><?php echo $fila['telefono']; ?></td>
+					<td><?php echo $fila['ciudad']; ?></td>
+					<td><?php echo $fila['correo']; ?></td>
+					<td><?php echo $fila['contraseña']; ?></td>
+					<td><a href="update.php?id=<?php echo $fila['id']; ?>"  class="btn__update" >Editar</a></td>
+					<td><a href="delete.php?id=<?php echo $fila['id']; ?>" class="btn__delete">Eliminar</a></td>
+				</tr>
+			<?php endforeach ?>
+
+		</table>
+	</div>
+</body>
+</html>
